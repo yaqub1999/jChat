e57837d2195d9690e0ce1bfa6b06bdb793359c6e
Yaqub <jorge.yaqub.1106@gmail.com>
2024-07-16 08:38:06 -0300

    primeiro commit

---
+++ src/main/java/com/jchat/gpt/GptApplication.java
@@ -0,0 +1,13 @@
+package com.jchat.gpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptApplication.class, args);
+	}
+
+}
